6eb2098d64bfdfbebf94d3ed1475b12741c366f6
JN <jared.bozh@gmail.com>
2023-10-24 22:01:02 +0800

    content rewrite2

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -27,45 +27,6 @@ const Content = () => {
                         <div>Check out some of my projects.</div>
                     </div>
 
-                    <div className="box">
-                        <div>Андрей Боженков</div>
-
-                        <div>reactJS dev</div>
-
-                        <div>Stack</div>
-
-                        <div>Меня зовут Андрей Боженков</div>
-
-                        <div>Я front-end TS/JS разработчик</div>
-
-                        <div>I love creating and expressing new ideas, through web, app design, typography, and more.</div>
-
-                        <div>I have an experience in interactive design, illustration, and more.</div>
-
-                        <div>I love to cook, going to the beach, animals, and fries. I’m looking for my next big challenge in UX/UI and branding</div>
-
-                        <div>Check out some of my projects.</div>
-                    </div>
-
-                    <div className="box">
-                        <div>Андрей Боженков</div>
-
-                        <div>reactJS dev</div>
-
-                        <div>Stack</div>
-
-                        <div>Меня зовут Андрей Боженков</div>
-
-                        <div>Я front-end TS/JS разработчик</div>
-
-                        <div>I love creating and expressing new ideas, through web, app design, typography, and more.</div>
-
-                        <div>I have an experience in interactive design, illustration, and more.</div>
-
-                        <div>I love to cook, going to the beach, animals, and fries. I’m looking for my next big challenge in UX/UI and branding</div>
-
-                        <div>Check out some of my projects.</div>
-                    </div>
 
                 </div>
 
